193d50f76ea3a668dda2b8ff9d339a4f2a107f54
Leonardo Barros <leonardo.barros@sapientag2.com.br>
2022-03-19 20:05:12 -0300

    1111

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Demo test new</title>
+    <title>Test 1</title>
     <script src="https://sdk.inbenta.io/chatbot/1/inbenta-chatbot-sdk.js"></script>
 </head>
 <body>
